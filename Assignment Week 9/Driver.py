2cc442d1050fae5391a33aea6b1cbe36d2984544
audreyfabiola <114067267+audreyfabiola@users.noreply.github.com>
2022-11-29 15:02:23 +0700

    Add files via upload

---
+++ Assignment Week 9/Driver.py
@@ -0,0 +1,15 @@
+from CircleCylinder import *
+
+def main():
+    # Run the circle class:
+    # c = Circle(1.0, "red")
+    # print(c.toString())
+
+    # Run the cylinder class:
+    c = Cylinder(1.0, "red", 1.0)
+    print(c.toString())
+
+
+
+if __name__ == "__main__":
+    main()
